566c5c2948fecb32545d6624f1e9dfaede038434
Jackie Zhu <64090163+jckpokimain@users.noreply.github.com>
2021-09-23 00:40:11 -0400

    w

--- lab1/pnginfo.c
+++ lab1/pnginfo.c
@@ -38,10 +38,10 @@ int get_png_data_IHDR(struct data_IHDR *out, FILE *fp, long offset, int whence){
       (uint32_t)chunk[2] << 8  |
       (uint32_t)chunk[3];
     out->width = w;
-    uint32_t h = (uint32_t)chunk[5] << 24 |
-      (uint32_t)chunk[6] << 16 |
-      (uint32_t)chunk[7] << 8  |
-      (uint32_t)chunk[8];
+    uint32_t h = (uint32_t)chunk[4] << 24 |
+      (uint32_t)chunk[5] << 16 |
+      (uint32_t)chunk[6] << 8  |
+      (uint32_t)chunk[7];
     out->height = h;
     return 1;
 }
