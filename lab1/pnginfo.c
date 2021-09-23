47fe860f8c9e62c9c27cb67df01b3f771cfc517d
Jackie Zhu <64090163+jckpokimain@users.noreply.github.com>
2021-09-22 21:11:05 -0400

    w

--- lab1/pnginfo.c
+++ lab1/pnginfo.c
@@ -36,12 +36,12 @@ int main(int argc, char *argv[]){
     FILE *f = fopen(argv[1], "rb");
     unsigned char buf[8];
     fread(buf, sizeof(buf), 1, f);
-    data_IHDR data = {0};
-    get_png_data_IHDR(&data, f, 0, 0);
-    if(is_png(buf, 8) == 1){
- //       char * tld = strrchr(argv[1], '/');
-        printf("%s: %d x %d", argv[1], data.width, data.height);
-    }
+//     data_IHDR data = {0};
+//     get_png_data_IHDR(&data, f, 0, 0);
+//     if(is_png(buf, 8) == 1){
+//  //       char * tld = strrchr(argv[1], '/');
+//         printf("%s: %d x %d", argv[1], data.width, data.height);
+//     }
     fclose(f);
     return 0;
 }
