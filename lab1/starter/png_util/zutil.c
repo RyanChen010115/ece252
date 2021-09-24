c9b0ca35748d22ee6c92ec017b49e800ce3cd060
Jackie Zhu <64090163+jckpokimain@users.noreply.github.com>
2021-09-24 19:48:07 -0400

    w

--- lab1/starter/png_util/zutil.c
+++ lab1/starter/png_util/zutil.c
@@ -11,6 +11,22 @@
 
 #include <stdio.h>
 #include "zutil.h"
+#include <string.h>
+#include <assert.h>
+
+#if defined(MSDOS) || defined(OS2) || defined(WIN32) || defined(__CYGWIN__)
+#  include <fcntl.h>
+#  include <io.h>
+#  define SET_BINARY_MODE(file) setmode(fileno(file), O_BINARY)
+#else
+#  define SET_BINARY_MODE(file)
+#endif
+
+#define CHUNK 16384  /* =256*64 on the order of 128K or 256K should be used */
+
+/* TYPEDEFS */
+typedef unsigned char U8;
+typedef unsigned long int U64;
 
 /**
  * @brief: deflate in memory data from source to dest.
