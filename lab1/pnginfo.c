46581dffc9c104f012e405a16643b42fdbd728b0
Jackie Zhu <64090163+jckpokimain@users.noreply.github.com>
2021-09-24 00:09:49 -0400

    w

--- lab1/pnginfo.c
+++ lab1/pnginfo.c
@@ -99,7 +99,7 @@ int main(int argc, char *argv[]){
         //     printf("%x", crc4[i]);
         // }
         // printf("\n%x\n", crc_val);
-        U32 crcTemp = (uint32_t)crc4[0] << 24 |
+        crcTemp = (uint32_t)crc4[0] << 24 |
             (uint32_t)crc4[1] << 16 |
             (uint32_t)crc4[2] << 8  |
             (uint32_t)crc4[3];
@@ -116,7 +116,7 @@ int main(int argc, char *argv[]){
         //     printf("%x", crc4[i]);
         // }
         // printf("\n%x\n", crc_val);
-        U32 crcTemp = (uint32_t)crc4[0] << 24 |
+        crcTemp = (uint32_t)crc4[0] << 24 |
             (uint32_t)crc4[1] << 16 |
             (uint32_t)crc4[2] << 8  |
             (uint32_t)crc4[3];
