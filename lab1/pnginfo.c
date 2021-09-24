bcb8b751129275a223f3dba982e7a40205abf887
Jackie Zhu <64090163+jckpokimain@users.noreply.github.com>
2021-09-23 21:10:08 -0400

    w

--- lab1/pnginfo.c
+++ lab1/pnginfo.c
@@ -58,9 +58,10 @@ int main(int argc, char *argv[]){
     get_png_data_IHDR(&data, f, 0, 0);
     if(is_png(buf, 8) == 1){
         char* tld = strrchr(argv[1], '/');
-        printf("%s: %d x %d", tld, data.width, data.height);
+        printf("%s: %d x %d\n", tld + 4, data.width, data.height);
     } else{
-
+        char* tld = strrchr(argv[1], '/');
+        printf("%s: Not a PNG file\n", tld + 4, data.width, data.height);
     }
     fclose(f);
     return 0;
