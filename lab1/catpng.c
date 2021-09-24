7905d70f30063d376191297cab88ae56b3e366a2
Jackie Zhu <64090163+jckpokimain@users.noreply.github.com>
2021-09-24 16:22:53 -0400

    e

--- lab1/catpng.c
+++ lab1/catpng.c
@@ -9,6 +9,7 @@
 
 typedef unsigned char U8;
 typedef unsigned int  U32;
+typedef unsigned long int U64;
 
 
 typedef struct data_IHDR {// IHDR chunk data 
@@ -22,6 +23,13 @@ typedef struct data_IHDR {// IHDR chunk data
     U8  filter;       /* only method 0 is defined for now */
     U8  interlace;    /* =0: no interlace; =1: Adam7 interlace */
 } data_IHDR;
+
+typedef struct chunk {
+    U32 length;  /* length of data in the chunk, host byte order */
+    U8  type[4]; /* chunk type */
+    U8  *p_data; /* pointer to location where the actual data are */
+    U32 crc;     /* CRC field  */
+} *chunk_p;
 
 int get_png_data_IHDR(struct data_IHDR *out, FILE *fp, unsigned char* buf){
     uint32_t w = (uint32_t)buf[4] << 24 |
@@ -42,39 +50,121 @@ int main(int argc, char *argv[]){
     const int NUM_FILES = argc - 1;
     FILE *wr = fopen("./result.png", "wb+");
     int height = 0;
-    int tLength = 0;
+    //int tLength = 0;
     unsigned char header[8];
     // unsigned char length[4];
-    unsigned char buf4[4];
-    unsigned char IHDR[17];
-    char (*ptrArr)[NUM_FILES];
+    // unsigned char buf4[4];
+    // unsigned char IHDR[17];
+    // chunk_p chunkPTR[NUM_FILES];
     for(int i = 1; i < argc; i++){
         FILE *f = fopen(argv[i], "rb");
         fread(header, sizeof(header), 1, f);
-        fread(buf4, sizeof(buf4), 1, f);
-        fread(IHDR, sizeof(IHDR), 1, f);
-        fread(buf4, sizeof(buf4), 1, f);
-        data_IHDR data = {0};
-        get_png_data_IHDR(&data, f, IHDR);
-        printf("\n%d\n", data.height);
-        height += data.height;
-        // end of IHDR
-        fread(buf4, sizeof(buf4), 1, f);
-        int l = (uint32_t)buf4[0] << 24 |
-            (uint32_t)buf4[1] << 16 |
-            (uint32_t)buf4[2] << 8  |
-            (uint32_t)buf4[3];
-        tLength += l;
-        ptrArr[i - 1] = malloc(sizeof(char) * l);
-        // store read value in static array then use the compress tool to store it in dynamic
-        fread(buf4, sizeof(buf4), 1, f);
+        // Reading from IHDR
+        U32 widthPTR[4];
+        U32 heightPTR[4];
+        U8 IHDRData[5];
+        U32 IHDRCRC[32];
+
+        fread(widthPTR, sizeof(widthPTR), 1, f);
+        fread(heightPTR, sizeof(heightPTR), 1, f);
+        fread(IHDRData, sizeof(IHDRData), 1, f);
+        fread(IHDRCRC, sizeof(IHDRCRC), 1, f);
+
+        height += *heightPTR;
+
+        
+        // fread(buf4, sizeof(buf4), 1, f);
+        // fread(IHDR, sizeof(IHDR), 1, f);
+        // fread(buf4, sizeof(buf4), 1, f);
+        // data_IHDR data = {0};
+        // get_png_data_IHDR(&data, f, IHDR);
+        // printf("\n%d\n", data.height);
+        // height += data.height;
+        // // end of IHDR
+        // fread(buf4, sizeof(buf4), 1, f);
+        // int l = (uint32_t)buf4[0] << 24 |
+        //     (uint32_t)buf4[1] << 16 |
+        //     (uint32_t)buf4[2] << 8  |
+        //     (uint32_t)buf4[3];
+        // tLength += l;
+        // ptrArr[i - 1] = malloc(sizeof(char) * l);
+        // // store read value in static array then use the compress tool to store it in dynamic
+        // fread(buf4, sizeof(buf4), 1, f);
 
         
         fclose(f);
     }
-    for(int i = 0; i < NUM_FILES; i++){
-        free(ptrArr[i]);
-    }
+    // for(int i = 0; i < NUM_FILES; i++){
+    //     free(ptrArr[i]);
+    // }
     fclose(wr);
     return 0;
 }
+
+// int main(int argc, char *argv[]){
+
+//     const int NUM_FILES = argc - 1;
+//     FILE *wr = fopen("./result.png", "wb+");
+//     int height = 0;
+//     int tLength = 0;
+//     unsigned char header[8];
+//     // unsigned char length[4];
+//     unsigned char buf4[4];
+//     unsigned char IHDR[17];
+//     char (*ptrArr)[NUM_FILES];
+
+
+//     struct dirent *dir;
+//     DIR *d;
+//     d = opendir(location);
+//     struct stat buf;
+//     char folder[256];
+//     int numPng = 0;
+//     char fileName[50];
+//     int index;
+
+//     if((dir = readdir(d))!=NULL){
+//         fileName = strcpy(dir->d_name);
+//         index = (int)(strstr(fileName,'.')-fileName-1);
+//         numPng = (fileName[index])-'0';
+//     }
+//     while ((dir = readdir(d)) != NULL){
+//         char *str_path = dir->d_name;
+//         if((str_path[index]-'0') > numPng){
+//             numPng = str_path[index]-'0';
+//         }
+//     }
+//     for (int i = 0; i<numPng;i++){
+//         fileName[index] = i+'0';
+//         FILE *f = fopen(fileName, "rb");
+
+//         fread(header, sizeof(header), 1, f);
+//         fread(buf4, sizeof(buf4), 1, f);
+//         fread(IHDR, sizeof(IHDR), 1, f);
+//         fread(buf4, sizeof(buf4), 1, f);
+//         data_IHDR data = {0};
+//         get_png_data_IHDR(&data, f, IHDR);
+//         printf("\n%d\n", data.height);
+//         height += data.height;
+//         // end of IHDR
+//         fread(buf4, sizeof(buf4), 1, f);
+//         int l = (uint32_t)buf4[0] << 24 |
+//             (uint32_t)buf4[1] << 16 |
+//             (uint32_t)buf4[2] << 8  |
+//             (uint32_t)buf4[3];
+//         tLength += l;
+//         ptrArr[i] = malloc(sizeof(char) * l);
+//         // store read value in static array then use the compress tool to store it in dynamic
+//         fread(buf4, sizeof(buf4), 1, f);
+
+//         //rest of for loop here
+
+
+//         fclose(f);
+//     }
+//     for(int i = 0; i < NUM_FILES; i++){
+//         free(ptrArr[i]);
+//     }
+//     fclose(wr);
+//     return 0;
+// }
