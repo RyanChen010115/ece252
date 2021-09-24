1768e15e53b35d22167696c3b4c137ce589198d4
Jackie Zhu <64090163+jckpokimain@users.noreply.github.com>
2021-09-24 19:15:15 -0400

    w

--- lab1/catpng.c
+++ lab1/catpng.c
@@ -43,7 +43,7 @@ int main(int argc, char *argv[]){
         fread(widthPTR, sizeof(U32), 1, f);
         fread(heightPTR, sizeof(U32), 1, f);
         fread(IHDRData, sizeof(IHDRData), 1, f);
-        fread(ICRC, sizeof(ICRC), 1, f);z
+        fread(ICRC, sizeof(ICRC), 1, f);
         U64 height = *heightPTR;
         U64 width = *widthPTR;
         height = ((height>>24)&0xff) | 
