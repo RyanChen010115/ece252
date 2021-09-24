b04acd762406735324fc730f219081b4eb8951d7
Jackie Zhu <64090163+jckpokimain@users.noreply.github.com>
2021-09-24 19:09:15 -0400

    w

--- lab1/catpng.c
+++ lab1/catpng.c
@@ -111,7 +111,7 @@ int main(int argc, char *argv[]){
         free(CRC);
         fclose(f);
     }
-    printf("%s", chunkPTR[0]->length);
+    printf("%d", chunkPTR[0]->length);
     // for(int i = 0; i < NUM_FILES; i++){
     //     free(ptrArr[i]);
     // }
