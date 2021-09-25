a0fd76136db0cf891c0ef1bbe4c703a7f1eb6378
Jackie Zhu <64090163+jckpokimain@users.noreply.github.com>
2021-09-24 20:28:48 -0400

    w

--- lab1/catpng.c
+++ lab1/catpng.c
@@ -74,7 +74,7 @@ int main(int argc, char *argv[]){
                     ((num<<8)&0xff0000) | 
                     ((num>>8)&0xff00) | 
                     ((num<<24)&0xff000000); 
-        printf("%ld\n", num);
+        printf("%d\n", num);
         tLength += num;
         U8* IDATdata = (U8*)malloc( num);
         fread(IDATdata, sizeof(U8) * num, 1, f);
@@ -89,7 +89,7 @@ int main(int argc, char *argv[]){
         chunk->type[2] = IDATtype[2];
         chunk->type[3] = IDATtype[3];
         chunk->p_data = (U8*)malloc(height*(width*4+1));
-        printf("\n%d\n", height*(width*4+1);
+        printf("\n%ld\n", height*(width*4+1);
         mem_inf(chunk->p_data, &lenUnComp, IDATdata, num);
         tLengthUC += lenUnComp;
 
