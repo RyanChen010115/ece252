12ede35da9f9ee533919b29ef8b900e123565faf
Jackie Zhu <64090163+jckpokimain@users.noreply.github.com>
2021-10-26 19:44:44 -0400

    w

--- lab2/paster.c
+++ lab2/paster.c
@@ -208,6 +208,9 @@ int catpng(int argc){
     fIDATchunk->type[2] = chunkPTR[0]->type[2];
     fIDATchunk->type[3] = chunkPTR[0]->type[3];
     fIDATchunk->p_data = fIDATdata;
+    for(int i = 0; i < 4; i++){
+        printf("%x\n", fIDATchunk->type[i]);
+    }
     U32 IDATcrc = getIDATcrc(fIDATchunk, IDATcomplength);
 
     //writing to file
