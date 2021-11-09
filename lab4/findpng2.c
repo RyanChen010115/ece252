e0c07aab65f1142e5972dbebc35e196bb1ff6d65
Jackie Zhu <64090163+jckpokimain@users.noreply.github.com>
2021-11-08 21:23:47 -0500

    w

--- lab4/findpng2.c
+++ lab4/findpng2.c
@@ -81,6 +81,7 @@ int recv_buf_init(RECV_BUF *ptr, size_t max_size);
 int recv_buf_cleanup(RECV_BUF *ptr);
 void cleanup(CURL *curl, RECV_BUF *ptr);
 int write_file(const char *path, const void *in, size_t len);
+int append_file(const char *path, const void *in, size_t len);
 CURL *easy_handle_init(RECV_BUF *ptr, const char *url);
 int process_data(CURL *curl_handle, RECV_BUF *p_recv_buf);
 
