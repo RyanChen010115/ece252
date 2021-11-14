a545eabc427cb9c3ced04bd37818da6297bc2b32
Jackie Zhu <64090163+jckpokimain@users.noreply.github.com>
2021-11-14 12:03:25 -0500

    w

--- lab4/findpng2.c
+++ lab4/findpng2.c
@@ -717,12 +717,13 @@ int main( int argc, char** argv )
         appendList(&visitedURLList, LOGFILE);
     }
     appendList(&visitedPNGList, PNGFILE);
-    // if (gettimeofday(&tv, NULL) != 0) {
-    //     perror("gettimeofday");
-    //     abort();
-    // }
-    // times[1] = (tv.tv_sec) + tv.tv_usec/1000000.;
-    // printf("paster2 execution time: %.6lf seconds\n", times[1] - times[0]);
+    
+    if (gettimeofday(&tv, NULL) != 0) {
+        perror("gettimeofday");
+        abort();
+    }
+    times[1] = (tv.tv_sec) + tv.tv_usec/1000000.;
+    printf("paster2 execution time: %.6lf seconds\n", times[1] - times[0]);
 
     
     freeList(&visitedURLList);
