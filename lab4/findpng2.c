326d2c4cbf6f457fc21ebacaaae0294703363071
Jackie Zhu <64090163+jckpokimain@users.noreply.github.com>
2021-11-14 13:04:58 -0500

    w

--- lab4/findpng2.c
+++ lab4/findpng2.c
@@ -587,9 +587,9 @@ void * crawler(void* variable){
         //need mutex
         if(toVisitURLList.head == NULL){
             pthread_mutex_unlock(&toVisitMutex);
-            if (neededPNG > uniquePNGNum){
-                continue;
-            }
+            // if (neededPNG > uniquePNGNum){
+            //     continue;
+            // }
             else{
                 break;
             }
