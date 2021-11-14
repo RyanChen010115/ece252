0653ab66152fb19cc0b4ab790d278b539854e11e
Jackie Zhu <64090163+jckpokimain@users.noreply.github.com>
2021-11-14 13:18:57 -0500

    w

--- lab4/findpng2.c
+++ lab4/findpng2.c
@@ -576,7 +576,7 @@ int process_data(CURL *curl_handle, RECV_BUF *p_recv_buf)
 void * crawler(void* variable){
     
     while (uniquePNGNum < neededPNG){
-        printf("1\n");
+        //printf("1\n");
         sem_wait(&foundSem);
         if (neededPNG <= uniquePNGNum){
             sem_post(&foundSem);
