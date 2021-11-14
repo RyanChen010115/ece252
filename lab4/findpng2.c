93e8396bc013c8104405c5352da061a46fccf2c5
Jackie Zhu <64090163+jckpokimain@users.noreply.github.com>
2021-11-14 10:06:18 -0500

    w

--- lab4/findpng2.c
+++ lab4/findpng2.c
@@ -762,7 +762,7 @@ int main( int argc, char** argv )
         pthread_cond_wait(&maxPNG,&pngMutex);
         for (int i = 0; i < numThreads; i++){
             pthread_join(pid[i],NULL);
-            printf("Thread %d exited\n", pid[i]);
+            printf("Thread %ld exited\n", pid[i]);
         }
     }
     pthread_mutex_unlock(&pngMutex);
